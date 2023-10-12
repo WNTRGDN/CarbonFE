e24ece4a642dff3c620fd0881d37f5a1db8e132e
Dion Gee <dion@wntr.com.au>
2023-10-12 21:27:38 +1000

    moved google tags from head inot body

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,9 +7,14 @@ export default function Document() {
 
   return (
     <Html lang="en">
-      { isProduction ? <Head><Script id="gtag" async src="https://www.googletagmanager.com/gtag/js?id=G-5RTJX54Y3E" /></Head> : <Head /> }
+      <Head />
       <body>
-        <Script id="g-tag" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-5RTJX54Y3E', { page_path: window.location.pathname });` }} />
+        { isProduction ?
+          <>
+            <Script id="gtag" async src="https://www.googletagmanager.com/gtag/js?id=G-5RTJX54Y3E" />
+            <Script id="g-tag" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-5RTJX54Y3E', { page_path: window.location.pathname });` }} />
+          </>
+        : null }
         <Main />
         <NextScript />
       </body>
