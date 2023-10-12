56ebd2daf5beed9ef87e588e78417d65852ebb39
Dion Gee <dion@wntr.com.au>
2023-10-12 18:47:26 +1000

    Readded in routing stuff

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
 import 'WNTR/styles/globals.scss'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  const router = useRouter()
+  
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      if (process.env.NODE_ENV === "production") {
+        window.gtag('config', 'G-5RTJX54Y3E', { page_path: url })
+      }
+    }
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    }
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
