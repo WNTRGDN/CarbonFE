4524a8e6d5a2e6b98bccb1e75922cbe835cddd9c
Dion Gee <dion@wntr.com.au>
2023-10-12 18:18:45 +1000

    Added in GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
 import 'WNTR/styles/globals.scss'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  const isProduction = process.env.NODE_ENV === "production"
+  const router = useRouter()
+  
+  useEffect(() => {
+    const handleRouteChange = (url: any) => {
+      if (isProduction) {
+        window.gtag('config', 'G-5RTJX54Y3E', { page_path: url })
+      }
+    }
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    }
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,17 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
+
+  const isProduction = process.env.NODE_ENV === 'production'
+
   return (
     <Html lang="en">
-      <Head />
+      { isProduction ?
+      <Head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-5RTJX54Y3E" />
+        <Script dangerouslySetInnerHTML={{ __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-5RTJX54Y3E', { page_path: window.location.pathname });` }} />
+      </Head> : <Head /> }
       <body>
         <Main />
         <NextScript />
