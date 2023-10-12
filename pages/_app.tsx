68799617af8b13ea70b80d194c3e6deed09e6cd1
Dion Gee <dion@wntr.com.au>
2023-10-12 21:15:01 +1000

    Checking url

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
   
   useEffect(() => {
     const handleRouteChange = (url: string) => {
+      console.log(url)
       if (process.env.NODE_ENV === "production" && typeof window !== 'undefined') {
         window.gtag('config', 'G-5RTJX54Y3E', { page_path: url })
       }
