b73b852075f6f636c7d541b82af0bb642deea22b
Dion Gee <dion@wntr.com.au>
2023-10-13 09:10:40 +1000

    Trying diff approach to gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,21 +2,20 @@ import 'WNTR/styles/globals.scss'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 import type { AppProps } from 'next/app'
+import * as gtag from "../utils/gtag"
 
 export default function App({ Component, pageProps }: AppProps) {
 
   const router = useRouter()
-  
+
   useEffect(() => {
-    const handleRouteChange = (url: string) => {
-      if (process.env.NODE_ENV === "production" && typeof window.gtag !== 'undefined') {
-        window.gtag('config', 'G-5RTJX54Y3E', { page_path: url })
-      }
-    }
-    router.events.on('routeChangeComplete', handleRouteChange);
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
     return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    }
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
   }, [router.events])
 
   return <Component {...pageProps} />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { GA_TRACKING_ID } from "../utils/gtag"
+
 export default function Document() {
-
-  const isProduction = process.env.NODE_ENV === 'production'
-
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+        <script dangerouslySetInnerHTML={{ __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', '${GA_TRACKING_ID}', { page_path: window.location.pathname });` }} />
+      </Head>
       <body>
         <Main />
         <NextScript />
