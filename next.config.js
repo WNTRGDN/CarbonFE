ab92115c77a1931fb15e8a9f778b85d57bf3fece
Dion Gee <dion@wntr.com.au>
2023-11-06 16:40:34 +1000

    Added in robots and sitemap

--- next.config.js
+++ next.config.js
@@ -2,6 +2,21 @@
 const nextConfig = {
     reactStrictMode: true,
     trailingSlash: true,
+    async headers() {
+      const headers = [];
+      if (process.env.NEXT_PUBLIC_VERCEL_ENV !== 'production') {
+        headers.push({
+          headers: [
+            {
+              key: 'X-Robots-Tag',
+              value: 'noindex',
+            },
+          ],
+          source: '/:path*',
+        });
+      }
+      return headers;
+    }
   }
   
   module.exports = nextConfig
--- pages/[[...slug]].tsx
+++ pages/[[...slug]].tsx
@@ -29,6 +29,7 @@ export default function Index({ website, page }: { website: IWebsite, page: IPag
         <meta name="twitter:description" content={page.metaData.description} />
         <meta name="twitter:image" content={`${page.metaData.image}?mode=crop&width=500&height=500`} />
         <meta name="environment" content={process.env.NEXT_PUBLIC_VERCEL_ENV} />
+        {process.env.NEXT_PUBLIC_VERCEL_ENV !== 'production' ? <meta name="robots" content="noindex,follow" /> : null }
         <link rel="canonical" href={page.url}></link>
       </Head>
       <Container fluid className='h-100'>
