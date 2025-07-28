f73d99b7895cf4f136b54c76199b116fad15c0c5
Dion Gee <dion@wntr.com.au>
2025-07-28 17:32:56 -0600

    Adding in patch to allow all so images load in australia.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,24 @@
 /** @type {import('next').NextConfig} */
+
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval';
+  object-src 'none';
+`;
+
 const nextConfig = {
     reactStrictMode: true,
     trailingSlash: true,
     async headers() {
-      const headers = [];
+      const headers = [{
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+          },
+        ],
+      }];
       if (process.env.NEXT_PUBLIC_VERCEL_ENV !== 'production') {
         headers.push({
           headers: [
