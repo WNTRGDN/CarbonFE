3564e6c6e9337a97364f57880addba13d3eab68c
Dion Gee <dion@wntr.com.au>
2025-07-28 19:06:26 -0600

    added tag manager to image allowed list

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const ContentSecurityPolicy = `
   default-src 'self';
   script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com;
   style-src 'self' 'unsafe-inline' https://use.typekit.net https://p.typekit.net;
-  img-src 'self' data: https://wntrau-001-site1.qtempurl.com https://www.google-analytics.com;
+  img-src 'self' data: https://wntrau-001-site1.qtempurl.com https://www.google-analytics.com https://www.googletagmanager.com;
   font-src 'self' https://use.typekit.net https://p.typekit.net;
   connect-src 'self' https://www.google-analytics.com;
 `;
