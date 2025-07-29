67dfd616931b2ea15b833a81e760ff14ad67071a
Dion Gee <dion@wntr.com.au>
2025-07-28 20:04:54 -0600

    Reinstated the old domain to get all the images saved.

--- blocks/jumbotron/index.tsx
+++ blocks/jumbotron/index.tsx
@@ -1,9 +1,9 @@
 import React, { FC } from 'react'
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap'
 
 const Jumbotron: FC<IJumbotron> = (jumbotron) => {
     return (
-        <article className={jumbotron.alias} style={jumbotron.image.length ? { backgroundImage:`url(${jumbotron.image.replace("https://wntrau-001-site1.qtempurl.com", "")})` } : {}}>
+        <article className={jumbotron.alias} style={jumbotron.image.length ? { backgroundImage:`url(${jumbotron.image})` } : {}}>
             <div className={`${jumbotron.alias}__inner`}>
                 <Container>
                     <Row>
